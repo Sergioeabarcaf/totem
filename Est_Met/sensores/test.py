ad58764f5bb5cdbafde2901908223c03c8061009
sergioeabarcaf <sergioaf1991@gmail.com>
2017-09-13 21:04:00 -0300

    sergioeabarcaf UPDATE test.py: Se modifica para ser ejecutado cada 10 segundo

--- Est_Met/sensores/test.py
+++ Est_Met/sensores/test.py
@@ -1,10 +1,13 @@
 import os
+import time
 
-os.system("python3 /home/pi/totem/Est_Met/sensores/altura.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/humedad.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/nivel_mar.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/presion.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/pun_rocio.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/temperatura_BMP085.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/temperatura.py")
-os.system("python3 /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
+while(True):
+    os.system("python3 /home/pi/totem/Est_Met/sensores/altura.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/humedad.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/nivel_mar.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/presion.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/pun_rocio.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/temperatura_BMP085.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/temperatura.py")
+    os.system("python3 /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
+    time.sleep(10)
