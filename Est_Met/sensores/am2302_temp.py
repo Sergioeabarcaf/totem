b4c0d66aec6d25c690fe786c32ac5f3005e67f23
sergioeabarcaf <sergioaf1991@gmail.com>
2017-10-05 11:35:08 -0300

    sergioeabarcaf UPDATE temYhum: Se trunca a dos decimales el valor enviado por mqtt

--- Est_Met/sensores/am2302_temp.py
+++ Est_Met/sensores/am2302_temp.py
@@ -7,8 +7,8 @@ host = "192.168.251.20" #Direccion IP de donde esta alojado el servidor MQTT
 port = "1883" #Puerto por defecto del servidor MQTT
 humedad, temperatura = sensor.read_retry(sensor.AM2302, 24)
 
-menTem = "totem1/" + str(temperatura)
-menHum = "totem1/" + str(humedad)
+menHum = "totem1/" + str(round(humedad,2))
+menTem = "totem1/" + str(round(temperatura,2))
 
 os.system("mosquitto_pub -t temperatura -m " + menTem + " -h " + host + " -p " + port)
 os.system("mosquitto_pub -t humedad -m " + menHum + " -h " + host + " -p " + port)
