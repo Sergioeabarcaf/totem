94c9e54766e8605791290dd5bec6cf92bf94fda4
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-10 16:36:10 -0300

    ajuste menor

--- Est_Met/sensores/test.py
+++ Est_Met/sensores/test.py
@@ -1,18 +1,4 @@
 import os
-import time
-
-while(True):
-    #horactual = time.time()
-    os.system("sudo python /home/pi/totem/Est_Met/sensores/am2302_temp.py")
-    #os.system("sudo python /home/pi/totem/Est_Met/sensores/altura.py")
-    #os.system("sudo python3 /home/pi/totem/Est_Met/sensores/humedad.py")
-    #os.system("sudo python /home/pi/totem/Est_Met/sensores/nivel_mar.py")
-    os.system("sudo python /home/pi/totem/Est_Met/sensores/presion.py")
-    #os.system("sudo python3 /home/pi/totem/Est_Met/sensores/pun_rocio.py")
-    #os.system("sudo python /home/pi/totem/Est_Met/sensores/temperatura_BMP085.py")
-    #os.system("sudo python3 /home/pi/totem/Est_Met/sensores/temperatura.py")
-    os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
-    #print(time.time() - horactual)
-    time.sleep(60)
-
-
+os.system("sudo python /home/pi/totem/Est_Met/sensores/am2302_temp.py")
+os.system("sudo python /home/pi/totem/Est_Met/sensores/presion.py")
+os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
