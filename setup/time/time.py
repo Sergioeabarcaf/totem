e141e6dca93fc2f0676d1028e6a1ccd3efaa19e3
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-10 16:35:39 -0300

    esperar 80 segundos antes de modificar la hora

--- setup/time/time.py
+++ setup/time/time.py
@@ -2,15 +2,9 @@ import os
 import time
 import sys
 
-os.system(ssh turismo@192.168.252.250 'date "+%Y%m%d %T"'`")
-
-"ssh turismo@192.168.252.250 'date "+%Y%m%d %T"'"
-
+time.sleep(80)
 date = open("time.txt")
 line = date.readline()
-print(line)
 date.close()
-
-time.sleep(3)
 os.system("sudo date +%Y%m%d%T -s '"+line+"'")
 sys.exit()
