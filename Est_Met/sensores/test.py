3b80151c5ee3c0517a5e83ddb1517b90206bde44
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-11 17:50:02 -0300

    se piden datos de la estacion al reiniciar la RPI y cada 30 minutos

--- Est_Met/sensores/test.py
+++ Est_Met/sensores/test.py
@@ -1,4 +1,7 @@
 import os
+import time
+
+time.sleep(30)
 os.system("sudo python /home/pi/totem/Est_Met/sensores/am2302_temp.py")
 os.system("sudo python /home/pi/totem/Est_Met/sensores/presion.py")
 os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
