9ae443d29698aa90b694dc2ea402b744895fedca
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-18 10:33:53 -0300

    parche de envio uv = 2

--- Est_Met/sensores/test.py
+++ Est_Met/sensores/test.py
@@ -1,7 +1,15 @@
 import os
 import time
+import sys
 
-time.sleep(60)
+host = "192.168.150.2"
+port = "1883"
+uv = 2
+men = "totem1/" + str(uv)
+
+os.system("mosquitto_pub -t uv -m " + men + " -h " + host + " -p " + port)
+#time.sleep(60)
 os.system("sudo python /home/pi/totem/Est_Met/sensores/am2302_temp.py")
 os.system("sudo python /home/pi/totem/Est_Met/sensores/presion.py")
-os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
+#os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
+
