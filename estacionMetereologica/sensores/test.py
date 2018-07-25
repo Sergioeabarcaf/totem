5fc33f32f225a8b4575ea6f62c0d6bf325a465f3
sergioeabarcaf <sergioaf1991@gmail.com>
2018-07-25 17:47:51 -0400

    Eliminacion de mensaje con UV=2

--- estacionMetereologica/sensores/test.py
+++ estacionMetereologica/sensores/test.py
@@ -4,12 +4,8 @@ import sys
 
 host = "192.168.150.2"
 port = "1883"
-uv = 2
-men = "totem1/" + str(uv)
 
-os.system("mosquitto_pub -t uv -m " + men + " -h " + host + " -p " + port)
-#time.sleep(60)
-os.system("sudo python /home/pi/totem/Est_Met/sensores/am2302_temp.py")
-os.system("sudo python /home/pi/totem/Est_Met/sensores/presion.py")
-os.system("sudo python /home/pi/totem/Est_Met/sensores/comunicacion_ok.py")
+os.system("sudo python /home/pi/totem/estacionMetereologica/sensores/am2302_temp.py")
+os.system("sudo python /home/pi/totem/estacionMetereologica/sensores/presion.py")
+os.system("sudo python /home/pi/totem/estacionMetereologica/sensores/comunicacion_ok.py")
 
