980062dbeb57b01680d889c88213e31492e50ceb
sergioeabarcaf <sergioaf1991@gmail.com>
2018-07-31 12:27:22 -0400

    se obtienen datos de sensores de prueba, se muestran por pantall, se almacena en log

--- estacionMetereologica/test-sensores/am2302_log.py
+++ estacionMetereologica/test-sensores/am2302_log.py
@@ -6,9 +6,14 @@ import Adafruit_DHT as sensor
 from datetime import datetime
 
 humedad, temperatura = sensor.read_retry(sensor.AM2302, 24)
+strHum = "humedad: "+str(humedad)
+strTem = "temperatura: "+str(temperatura)
+strLog = "\n"+str(datetime.now())+" "+ strHum + strTem
 
-log=open("temp.log","ab")
-log.write("\n"+str(datetime.now())+" "+"temperatura: "+str(temperatura))
+log=open("log/am2302.log","ab")
+log.write(strLog)
 log.close()
+
+print strLog
 
 sys.exit()
--- estacionMetereologica/test-sensores/dht11_log.py
+++ estacionMetereologica/test-sensores/dht11_log.py
@@ -6,9 +6,12 @@ import Adafruit_DHT as sensor
 from datetime import datetime
 
 humedad, temperatura = sensor.read_retry(sensor.DHT11, 21)
+strPrint = "\n"+str(datetime.now())+" "+"temperatura: "+str(temperatura)+" "+"humedad: "+str(humedad)
 
-log=open("temp.log","ab")
-log.write("\n"+str(datetime.now())+" "+"temperatura: "+str(temperatura)+" "+"humedad: "+str(humedad))
+log=open("log/tempDHT11.log","ab")
+log.write(strPrint)
 log.close()
+
+print strPrint
 
 sys.exit()
