f0c745e0be591ef347064ffc9885705586639c77
sergioeabarcaf <sergioaf1991@gmail.com>
2018-11-12 17:18:11 -0300

    prueba de envio de datos a firebase

--- estacionMetereologica/sensores/sendFirebase.py
+++ estacionMetereologica/sensores/sendFirebase.py
@@ -2,11 +2,33 @@ import am2302
 import bmp180
 import comunicacion_ok
 
-data = {}
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import datetime
 
+import datetime
+
+def send(data):
+    ref = db.reference('data')
+    ref.push(data)
+
+data = {}
 data.update(bmp180.getPress())
 data.update(am2302.getTemHum())
 data.update(comunicacion_ok.getArduino())
+data.update('timestamp':datetime.datetime.utcnow())
+
+cred = credentials.Certificate('keyTurismo.json')
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://turismo-5351f.firebaseio.com/'
+})
+
+
+
+
+
+
 
 
 print data
