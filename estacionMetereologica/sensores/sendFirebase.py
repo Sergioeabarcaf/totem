f9b0dce1c74af6a3a89ebf1b5cc094bd4bd23072
sergioeabarcaf <sergioaf1991@gmail.com>
2018-11-12 17:03:27 -0300

    se pasa de usar append a update

--- estacionMetereologica/sensores/sendFirebase.py
+++ estacionMetereologica/sensores/sendFirebase.py
@@ -4,9 +4,9 @@ import comunicacion_ok
 
 data = {}
 
-data.append(bmp180.getPress())
-data.append(am2302.getTemHum())
-data.append(comunicacion_ok.getArduino())
+data.update(bmp180.getPress())
+data.update(am2302.getTemHum())
+data.update(comunicacion_ok.getArduino())
 
 
 print data
