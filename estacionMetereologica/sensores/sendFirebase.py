4e05b88dda6824df5016d21a9992122795325826
sergioeabarcaf <sergioaf1991@gmail.com>
2018-11-12 16:59:38 -0300

    se concentra todo en un solo diccionario data

--- estacionMetereologica/sensores/sendFirebase.py
+++ estacionMetereologica/sensores/sendFirebase.py
@@ -2,7 +2,11 @@ import am2302
 import bmp180
 import comunicacion_ok
 
+data = {}
 
-print bmp180.getPress()
-print am2302.getTemHum()
-print comunicacion_ok.getArduino()
+data.append(bmp180.getPress())
+data.append(am2302.getTemHum())
+data.append(comunicacion_ok.getArduino())
+
+
+print data
